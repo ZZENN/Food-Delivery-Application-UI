8f57e74043ff08efb30c3680999b0894c3ae74dc
ZZENN <zenphillip0@gmail.com>
2021-03-19 14:08:11 -0400

    2nd Edit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_food_delivery_ui/widgets/recent_orders.dart';
 import '../data/data.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -52,6 +52,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
         ),
+        RecentOrders(),
       ],
       ),
     );
